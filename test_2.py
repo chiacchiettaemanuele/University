9268df6bf428abd2d87024aed11f051aca6b688d
emanuele chiacchietta <chiacchietta.emanuele@gmail.com>
2026-05-08 00:03:08 +0200

    Local variables

---
+++ test_2.py
@@ -0,0 +1,10 @@
+def calcola_somma_interna():
+    primo_numero = 10
+    secondo_numero = 50
+
+    somma = primo_numero + secondo_numero
+    return somma
+
+
+risultato_finale = calcola_somma_interna()
+print(calcola_somma_interna())

90c198ee91facc37c2647eba8615bcdf5a05a991
emanuele chiacchietta <chiacchietta.emanuele@gmail.com>
2026-05-07 23:56:35 +0200

    Global variables and function

---
+++ test_1.py
@@ -0,0 +1,15 @@
+#Global variable
+
+a=5
+b=6
+
+#Function declaration
+def risultato(a,b):
+    somma=a+b
+    return somma
+
+def main():
+# Function call in the Main
+        finale=risultato(a,b)
+        print(finale)
+main()

93a2bffed39b65893e2f9246609e32a34e99b956
emanuele chiacchietta <chiacchietta.emanuele@gmail.com>
2026-05-08 13:15:11 +0200

    Type

--- test_3.py
+++ test_3.py
@@ -4,7 +4,7 @@ b=5.4
 print(type(b))
 c="world"
 print(type(c))
-list=[1,2,3,4,5]        #MUTABILE
+my_list=[1,2,3,4,5]        #MUTABILE
 print(type(list))
-list_2=(1,2,3,4,5)      #IMMUTABILE
+my_list_2=(1,2,3,4,5)      #IMMUTABILE
 print(type(list_2))

f9347c2239e12701cabdab48ed670d47fe896445
emanuele chiacchietta <chiacchietta.emanuele@gmail.com>
2026-05-08 17:50:48 +0200

    fixed-point.py

---
+++ fixed-point.py
@@ -0,0 +1,8 @@
+pi = 3.14159
+print(f"{pi:.2f}")  # Mostra solo 2 decimali
+# Output: 3.14
+
+
+stipendio = 50000
+print(f"{stipendio:,d}") # La 'd' indica un numero intero (decimal integer)
+# Output: 50,000

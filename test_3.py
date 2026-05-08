274b737cd48215687f71ea805abbf1731e550a5e
emanuele chiacchietta <chiacchietta.emanuele@gmail.com>
2026-05-08 13:09:34 +0200

    Type

---
+++ test_3.py
@@ -0,0 +1,10 @@
+a=5
+print(type(a))
+b=5.4
+print(type(b))
+c="world"
+print(type(c))
+list=[1,2,3,4,5]        #MUTABILE
+print(type(list))
+list_2=(1,2,3,4,5)      #IMMUTABILE
+print(type(list_2))

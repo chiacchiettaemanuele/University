3d16a289756d39a4a90ad60932dadec55089f0ad
emanuele chiacchietta <chiacchietta.emanuele@gmail.com>
2026-05-10 19:51:37 +0200

    the greater

---
+++ the greater.py
@@ -0,0 +1,8 @@
+number_1=float(input("first number ,press enter: "))
+number_2=float(input("second number ,press enter: "))
+if number_1 > number_2:
+    print("number 1 > number 2")
+elif number_1 == number_2:
+     print("number 1 == number 2")
+else:
+      print("number 1 < number 2")

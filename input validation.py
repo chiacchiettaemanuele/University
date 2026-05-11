d6ff64c255c779c06e0336a46850382e814fcc56
emanuele chiacchietta <chiacchietta.emanuele@gmail.com>
2026-05-11 15:23:01 +0200

    input validation

---
+++ input validation.py
@@ -0,0 +1,9 @@
+def main ():
+    a=int(input("enter first number"))
+    b=int(input("enter second number"))
+    while b==0:
+        b =int (input  ("insert number "))
+    print (a/float(b))
+main()
+
+
